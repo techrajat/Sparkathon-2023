39e575924fd100c8841f3f5e32ba98958513ab11
Rajat <rajatkumar100095@gmail.com>
2023-08-11 23:31:40 +0530

    Deals section made

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,13 @@
 import Deals from "./components/Deals";
-import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="bg-[#e6f1fc]">
-      {/* Navbar */}
       <Navbar />
-      {/* Hero */}
       <Hero />
-      {/* Deals */}
       <Deals />
-      {/* For You */}
-      {/* Footer */}
-      <Footer />
     </div>
   );
 }
